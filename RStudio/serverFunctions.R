ff6351a700c8793c3a40d10701df68b4a7f24b13
joaoguedes <joaoguedes.cjp@gmail.com>
2022-05-09 19:12:25 +0100

    Attempt to create plumber API with model
    Attempt to create plumber API with model

--- RStudio/serverFunctions.R
+++ RStudio/serverFunctions.R
@@ -4,9 +4,9 @@ source("serverFunc.R")
 #* @get /predict
 function(week){
   # 1_2_3_4_5_6_7
-  cona = predict(week)
+  res = predict(week)
   # res=paste("You are Predicting values for: ",week,sep = "")
   #res = c(tr,ts)
   #print(res)
-  return(cona)
+  return(res)
 }
